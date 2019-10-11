1880bd978584b33a72c8fae2ea226f95ddb93640
wubet <wube.tuffa@gmail.com>
2019-10-11 14:48:33 -0700

    Initial commit

---
+++ AzureCloudManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureCloudManager.Startup))]
+namespace AzureCloudManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
